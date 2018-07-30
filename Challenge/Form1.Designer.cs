3c4a3c5bdd3a6945aea2dd492a550261b42bb162
PabloZP <41882079+PabloZP@users.noreply.github.com>
2018-07-29 23:14:49 -0500

    Add files via upload

---
+++ Challenge/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Challenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCadena = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResul = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCadena
+            // 
+            this.txtCadena.Location = new System.Drawing.Point(18, 16);
+            this.txtCadena.Name = "txtCadena";
+            this.txtCadena.Size = new System.Drawing.Size(322, 20);
+            this.txtCadena.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResul
+            // 
+            this.txtResul.Location = new System.Drawing.Point(18, 81);
+            this.txtResul.Multiline = true;
+            this.txtResul.Name = "txtResul";
+            this.txtResul.Size = new System.Drawing.Size(394, 201);
+            this.txtResul.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(776, 303);
+            this.Controls.Add(this.txtResul);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCadena);
+            this.Name = "Form1";
+            this.Text = "Challenge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtCadena;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResul;
+    }
+}
+
---
+++ Challenge/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BE;
+using BL;
+
+namespace Challenge
+{
+    public partial class Form1 : Form
+    {
+       
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cadena = txtCadena.Text.Trim();
+            Texto C = new Texto(cadena);
+            C.ParsingText();
+            string sParseMssg = C.Message;
+            string sSearchMsg = "";
+            if (C.ParsedOK)
+            {
+                Searching SSS = new Searching(C.Words);
+                sSearchMsg = Environment.NewLine + SSS.Message;
+            }
+            txtResul.Text = sParseMssg + sSearchMsg;      
+        }
+
+
+        
+
+
+    }
+}
