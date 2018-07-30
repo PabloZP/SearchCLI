9def9d8982593816cee397625059b0c0ccf2a188
PabloZP <41882079+PabloZP@users.noreply.github.com>
2018-07-29 23:08:44 -0500

    Add files via upload

---
+++ ChallengeCLI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using BL;
+
+namespace ChallengeCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "";
+            for (int i = 0; i <= args.Length - 1; i++)
+            {
+                string elm = args[i].Trim();
+                Texto Tx = new Texto();
+                elm = Tx.StringWithoutAdditionalSpaces(elm);
+                if (elm.Contains(" "))
+                {
+                    elm = "\"" + elm + "\"";
+                }
+                cadena = cadena + " " + elm;
+            }
+            Texto C = new Texto(cadena);
+            C.ParsingText();
+            string sParseMssg = C.Message;
+            string sSearchMsg = "";
+            if (C.ParsedOK)
+            {
+                Searching SSS = new Searching(C.Words);
+                sSearchMsg = Environment.NewLine + SSS.Message;
+            }
+            Console.WriteLine(sParseMssg + sSearchMsg);
+
+            
+        }
+    }
+}
