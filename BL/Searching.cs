fd1bab62134eb020e5d9102d6d1fc29dfdea0b8e
PabloZP <41882079+PabloZP@users.noreply.github.com>
2018-07-29 23:11:52 -0500

    Add files via upload

---
+++ BL/Searching.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace BL
+{
+    public class Searching
+    {
+        public List<ProgLang> ListProgLangs { get; set; }
+        public List<SearchEngine> ListEngines { get; set; }
+        public bool SearchOk { get; set; }
+
+        public List<Search> ListSearchs { get; set; }
+        public ProgLang TotalWinner { get; set; }
+
+        public string Message { get; set; }
+
+        public Searching()
+        {
+            ListEngines = new List<SearchEngine>();
+            ListProgLangs = new List<ProgLang>(); 
+            ListSearchs = new List<Search>();
+        }
+
+        public Searching(List<string> ListaElms)
+        {
+            ListEngines = new List<SearchEngine>(); 
+
+            SearchEngine GE = new GoogleEngine("Google"); // using Polymorphism
+            ListEngines.Add(GE);
+
+            SearchEngine YE = new YahooEngine("Yahoo"); // using Polymorphism
+            ListEngines.Add(YE);
+
+            ListSearchs = new List<Search>(); 
+
+            ListProgLangs = LisStringToLisProgLang(ListaElms);
+            SearchOk = bSearchNumElmsOk();
+            if (SearchOk)
+            {
+                try {
+                    //BuildSearchs
+                    foreach (ProgLang oPr in ListProgLangs)
+                    {
+                        Search oSearch = new Search(GE, oPr, 0);
+                        oSearch.NumResults = GE.GetResults(oPr.NomLang);    
+                        ListSearchs.Add(oSearch);
+                    }                  
+                    foreach (ProgLang oPr in ListProgLangs)
+                    {
+                        Search oSearch = new Search(YE, oPr, 0);
+                        oSearch.NumResults = YE.GetResults(oPr.NomLang);   
+                        ListSearchs.Add(oSearch);
+                    }
+                    // Doing calculations
+                    Calc();
+                    // Showing results
+                    Message = ShowAllResults();
+                }
+                catch
+                {
+                    SearchOk = false;
+                    Message = "Error in performing search in Search Engine";
+                }
+                
+            }
+            else
+            {
+                Message = "In order to perform a comparison, must be at least 2 items to be compared";
+            }
+        }
+
+        string ShowAllResults()
+        {
+            string r = "";
+            foreach (ProgLang oPL in ListProgLangs)
+            {
+                List<Search> L = ListSearchs.Where(x => x.Lang.NomLang== oPL.NomLang).ToList();
+                string s = "";
+                foreach (Search oSea in L)
+                {
+                    s = s + " " + oSea.Engine.NomSearch + ": " + oSea.NumResults.ToString();
+                }
+                r = r + oPL.NomLang + ": " + s + " * Tot. Res. of prog. lang.: " + oPL.TotResults +  Environment.NewLine;
+            }
+            string w = "";
+            foreach (SearchEngine oSeEng in ListEngines)
+            {
+                w = w + "\n" + oSeEng.NomSearch + " Winner: "  + oSeEng.Winner.NomLang +  Environment.NewLine;  
+            }
+            return r + w + "\n" + "Total Winner: " + this.TotalWinner.NomLang;
+        }
+
+
+        List<ProgLang> LisStringToLisProgLang(List<string> LS)
+        {
+            List<ProgLang> LPL = new List<ProgLang>();
+            foreach (string s in LS)
+            {
+                ProgLang PL = new ProgLang(s);
+                LPL.Add(PL); 
+            }
+            return LPL;    
+        }
+
+        public void SetLisSearchs()
+        {
+            ListSearchs = new List<Search>();
+            foreach (SearchEngine SE in ListEngines)
+            {
+                foreach (ProgLang PL in ListProgLangs)
+                {
+                    Search Sea = new Search(SE, PL, 0);
+                }
+            }
+        }
+
+        public void Calc()
+        {
+            SetWinnersOfEachEngine();
+            SetTotResOfEachProgLang();
+            SetTotalWinner();
+        }
+
+        public void SetWinnersOfEachEngine()
+        {
+            foreach (SearchEngine o in ListEngines)
+            {
+                o.Winner = ProgLangWinnerOfEngine(o); 
+            }
+        }
+
+        public void SetTotResOfEachProgLang()
+        {
+            foreach (ProgLang o in ListProgLangs)
+            {
+                o.TotResults = TotResultsOfProgLang(o); 
+            }
+        }
+
+        public void SetTotalWinner()
+        {
+            int nMax = 0;
+            ProgLang AuxWinner = new ProgLang(); 
+            foreach (ProgLang o in ListProgLangs)
+            {
+                if (o.TotResults > nMax)
+                {
+                    AuxWinner = o;
+                    nMax = AuxWinner.TotResults;   
+                }
+            }
+            TotalWinner = AuxWinner; 
+        }
+
+        ProgLang ProgLangWinnerOfEngine(SearchEngine oEng)
+        {
+            ProgLang PrgLng = new ProgLang();
+            List<Search> L = ListSearchs.Where(x => x.Engine.NomSearch == oEng.NomSearch).ToList();
+            int nMax = 0;
+            ProgLang PrgAux = new ProgLang(); 
+            foreach (Search o in L)
+            {
+                if (o.NumResults > nMax)
+                {
+                    PrgAux = new ProgLang(o.Lang.NomLang);
+                    nMax = o.NumResults; 
+                }             
+            }
+            PrgLng = PrgAux; 
+            return PrgLng;
+        }
+
+        int TotResultsOfProgLang(ProgLang oPr)
+        {
+            int Sum = 0;
+            List<Search> L = ListSearchs.Where(x => x.Lang.NomLang == oPr.NomLang).ToList();
+            foreach (Search o in L)
+            {
+                Sum = Sum + o.NumResults; 
+            }
+            return Sum;
+        }
+
+        bool bSearchNumElmsOk()
+        {
+            bool bLangOK = ((2 <= ListProgLangs.Count)); // PDT for app makes sense ther must be agt least 2 langs to COMPARE
+            bool bEngineOK = ((2 <= ListEngines.Count)); 
+            return bLangOK && bEngineOK;
+        }
+
+    
+    }
+}
---
+++ BL/Texto.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BE;
+
+namespace BL
+{
+    public class Texto
+    {
+        public string TxtToBeParsed { get; set; }
+        public List<string> Words { get; set; }
+        public string Message { get; set; }
+        public bool ParsedOK { get; set; }
+
+        public Texto()
+        {
+            Words = new List<string>();
+        }
+
+        public Texto(string Txt)
+        {
+            Words = new List<string>();
+            TxtToBeParsed = Txt; 
+        }
+
+        public void ParsingText()
+        {
+            TxtToBeParsed = TxtToBeParsed.Trim(); 
+            TxtToBeParsed = StringWithoutAdditionalSpaces(TxtToBeParsed);
+            if (TxtToBeParsed.Length > 0)
+            {
+                ParsedOK = bTextOk(TxtToBeParsed.Trim());
+                if (ParsedOK)
+                {
+                    if (HasSpaceBetweenQuotes(Words))
+                    {
+                        ParsedOK = false;
+                        Message = "One or more elements Has One Space Between Quotes";
+                    }
+                }
+            }
+            else
+            {
+                Message = "You entered Empty String";
+                ParsedOK = false; 
+            }
+        }
+
+        bool bTextOk(string s)
+        {
+            bool b = false;
+            int NumQuo = NumQuotes(s);
+            if (NumQuo == 0)
+            {
+                b = true;
+                Message = "Parsed OK -> No Quotes";
+                // 1.A. OK Numero de Comillas = 0  --> caso facil, se usa Split
+                Words = ListWordsNoQuotes(s); 
+            }
+            else
+            {
+                if ((NumQuo % 2) == 0)
+                {
+                    // NUM COMILLAS PAR:
+                    if (HasQuotesContiguas(s))
+                    {
+                        Message = "ERR -> Double quotes can not be together";
+                    }
+                    else
+                    {
+                        if (StringWithQuotesWithStickLetter(s))
+                        {
+                            Message = "ERR -> Character together to double quotes";
+                        }
+                        else
+                        {
+                            b = true;
+                            Message = "Parsed OK -> With Double Quotes";
+                            // 1.B. OK Numero de Comillas > 0 y Par y OK 
+                            int contgral = 0;
+                            List<string> L = new List<string>();    
+                            ListWordsQuotes(s, contgral, ref L);
+                            Words = L;
+                        }
+                    }
+
+                }
+                else
+                {
+                    // 1.E.ERR Numero de Comillas > 0 e Impar  --> ERROR
+                    Message = "ERR -> Odd number of double quotes";
+                }
+            }
+            return b;
+        }
+
+        // 0. Trim
+        // 1. check 
+        // 1.A. Parsed OK -> Without quotes
+        // 1.B. Parsed OK -> With quotes y OK  --> ANALISIS
+        // 1.C. ERR--> Wiyth even number of  quotes but with character together with quotes
+        //  .... Example  "C Sharp"Java 
+        //  .... Example   Java"C Sharp" 
+        // 1.D. ERR -> Even number of quotes but contiguosu quotes -->  ERROR ... Exm  "C Sharp""Ruby Rails"
+        // 1.E. ERR -> Odd number of quotes
+        // OJO: Before Analizing all extra contiguous spaces are reduced to one space
+        //    EJM:   "A  B" CD    EF   --->   "A B" CD EF
+
+        public bool StringWithQuotesWithStickLetter(string s)
+        // detecta err 1.C
+        // Rule1: After closing quote (Even) must be space (unless closing quote is the last char)
+        // Rule2: Before opening quote (Odd) must be space (unless opening quote is the first char)
+        // PreCondition: String enters with even number of quotes (WELL FORMED)
+        {
+            bool b = false; 
+            //  1. get list of 1.A if it0s OP OR CL and its **abs pos* in the string
+            //  2. lopp list and check rules 1 and 2 foreach elm
+            string OpenOrClose = "OPE"; // OPE or CLO
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (s.Substring(i, 1) == "\"" && OpenOrClose == "OPE")
+                {
+
+                    if ((i > 0) && (s.Substring(i - 1, 1) != " "))
+                    {
+                        b = true; 
+                    }
+                    OpenOrClose = "CLO";
+                }
+                else
+                {
+                    if (s.Substring(i, 1) == "\"" && OpenOrClose == "CLO")
+                    {
+
+                        if ((i < (s.Length - 1)) && (s.Substring(i + 1, 1) != " "))
+                        {
+                            b = true; 
+                        }
+                        OpenOrClose = "OPE";
+                    }
+                }
+            }
+            return b;
+        }
+
+        public bool HasQuotesContiguas(string s) 
+        {
+            bool b = false;
+            if (s.Contains("\"\"") || s.Contains("\"\"\"") || s.Contains("\"\"\"\""))
+            {
+                b = true; 
+            }
+            return b;
+        }
+
+        public bool HasSpaceBetweenQuotes(List<string> L)  // If it has a " "
+        { 
+            // this apply to a well formed list         
+            bool b = false;
+            foreach (string s in L)
+            {
+                if (s.Contains("\" \""))
+                {
+                    b = true;
+                }
+            }
+            return b;
+        }
+
+        // 1.A OK->  No Quotes  
+        List<string> ListWordsNoQuotes(string s)
+        {
+            List<string> L = new List<string>();
+            string[] Arr = s.Split(' ');
+            L = Arr.ToList(); 
+            return L;
+        }
+
+        // 1.B. OK-> With Quotes  --> ANALISIS
+        public void ListWordsQuotes(string s, int contgral, ref List<string> L)
+        {
+            string sAux = "";
+            int PosNext = 0;
+            string sFirst = s.Substring(0, 1);
+
+            if (sFirst == "\"")
+            {
+                int i = 0 + 1; // from next one, doesnt include quote
+                contgral = i;
+                while (s.Substring(i, 1) != "\"")
+                {
+                    sAux = sAux + s.Substring(i, 1);
+                    i++;
+                    contgral++; 
+                }
+                PosNext = i + 2;
+                sAux = "\"" + sAux + "\"";
+            }
+            else  // character  not quote
+            {
+                int i = 0;  // CurPos
+                contgral = i;
+                while (s.Substring(i, 1) != " ")
+                {
+                    sAux = sAux + s.Substring(i, 1);
+                    i++;
+                    contgral++;
+                    if (i == s.Length)
+                    {
+                        break; 
+                    }
+                }
+                PosNext = i + 1;
+            }
+            L.Add(sAux);
+
+            if (contgral <= s.Length -1 && PosNext < s.Length)
+            {
+                string s2 = s.Substring(PosNext);
+                ListWordsQuotes(s2, contgral, ref L);
+            }
+        }
+
+
+
+        int NumQuotes(string s)
+        {
+            int n = 0;
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (s.Substring(i, 1) == "\"")
+                {
+                    n++;
+                }
+            }
+            return n;
+        }
+
+        public string StringWithoutAdditionalSpaces(string s)
+        {
+            string sRes = "";
+            Regex trimmer = new Regex(@"\s\s+");
+            sRes = trimmer.Replace(s, " ");
+            return sRes;
+        }
+
+
+    }
+}
